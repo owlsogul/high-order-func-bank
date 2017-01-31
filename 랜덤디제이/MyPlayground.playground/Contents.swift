bb1416adf92fc5d822e1fdc82117f6a72673b617
Kwon Byung Soo <planon21@gmail.com>
2017-01-31 12:36:03 +0900

    add

--- 랜덤디제이/MyPlayground.playground/Contents.swift
+++ 랜덤디제이/MyPlayground.playground/Contents.swift
@@ -4,7 +4,7 @@ import UIKit
 
 
 
-//1
+//문제 1 부분 출력, 부분 합
 let Numbers = [4, 5, 6, 7, 10, 9, 2, 11]
 var sum:Int=0
 for item in Numbers{
@@ -15,27 +15,68 @@ for item in Numbers{
     }else {
         
         sum += item
-            
+        
     }
 }
 print(sum)
 
 
-//2
-let score = [84, 59, 90, 42, 100, 75]
-func order(s1 : Int, s2 : Int) -> Bool {
-    return s1 < s2
+
+
+//문제 2. 곱하기
+let prices = [100.0, 110.0, 1150.0]
+var tax:[Double] = []
+for price in prices{
+    tax.append(price * 1.1)
 }
-var orderedScore = score.sorted(by: order)
-for item in orderedScore{
-    
-    if(item > 80){
-        print("A")
-    }else if (item < 80 && item > 30){
-        print("B")
-    }else if (item < 30 && item > 0){
-        print("C")
+print(tax)
+
+//문제 3. 음수만 양수로 만들기
+let someInts: [Int] = [-1, -192, 123, -34958, 1289]
+var positiveInts: [Int] = []
+for item in someInts {
+    if (item < 0){
+        positiveInts.append(item * -1)
     }
-    
-    
 }
+print(positiveInts)
+
+
+
+//문제 4. 정렬문제
+var nonOrderedInts:[Int] = [2,2,5,7,4,3,9,-1]
+
+
+var i:Int = 0
+var j:Int = 0
+while i < nonOrderedInts.endIndex {
+    while j < nonOrderedInts.endIndex {
+        
+        if nonOrderedInts[i] < nonOrderedInts[j]{
+            var tmp: Int = nonOrderedInts[i]
+            nonOrderedInts[i] = nonOrderedInts[j]
+            nonOrderedInts[j] = tmp
+        }
+        
+        j += 1
+    }
+    j = 0
+    i += 1
+}
+
+print(nonOrderedInts)
+
+
+//문제 5. smallints에서 8보다 작은 수를 얻고 싶다.
+
+let someints = [4,8,13,15,3]
+
+var smallerthaneight: [Int] = []
+
+for item in someints {
+    if item < 8 {smallerthaneight.append(item)
+        print(item)
+    }
+}
+
+
